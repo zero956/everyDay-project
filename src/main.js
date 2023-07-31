9065aecc522301ee8408a1d1f973505ef12b7de3
lph <1751443140@qq.com>
2023-07-31 17:53:09 +0800

    系统初始布局

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { Button, Menu, Icon, Breadcrumb } from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+Vue.component(Button.name, Button);
+Vue.component(Menu.name, Menu);
+Vue.component(Icon.name, Icon);
+Vue.component(Menu.Item.name, Menu.Item)
+Vue.component(Menu.SubMenu.name, Menu.SubMenu)
+Vue.component(Breadcrumb.name, Breadcrumb)
+Vue.component(Breadcrumb.Item.name, Breadcrumb.Item)
 
 Vue.config.productionTip = false
 
